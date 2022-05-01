fbcea7532fdc33dab41c6248e3f7150153a0915d
dfbq91 <dfbq91@gmail.com>
2022-04-30 20:52:21 -0500

    Print a hello world

---
+++ c-program-structure/CProgramStructure.c
@@ -0,0 +1,7 @@
+#include <stdio.h> /* Directiva del preprocesador */
+
+int main() /* Función main: Punto de partida de nuestro programa en C */
+{
+    printf("Hello world");
+    return 0; /* Fin de nuestro programa */
+}
