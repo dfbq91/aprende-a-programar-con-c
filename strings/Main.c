674704c7e12c2ca22e9c50275c8cbeab38589a2a
dfbq91 <dfbq91@gmail.com>
2022-05-22 12:13:30 -0500

    Update example

--- strings/Main.c
+++ strings/Main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 
-void modify_string(char *c) {
+void modify_string(char c[]) {
     c[0] = 'X';
 }
 
@@ -15,42 +15,35 @@ int main() {
     /* These are all strings */
     printf("a\n");
     printf("Hello\n");
-    printf("'Hello world' are the most printed words when you are learning programming\n");
+    printf("'Hello world' are the most printed words when you are learning programming\n\n");
 
-    /* A string can be declarate in different ways */
+    /* There are different ways to declare a string */
 
     /* First way */
-    char my_str[6] = { 'H', 'E', 'L', 'L', 'O', '\0' };
-    printf("The length of the string is %li\n", strlen(my_str));
-    printf("My string is %s\n", my_str);
+    char my_str[6] = {'H', 'E', 'L', 'L', 'O', '\0'};
+    printf("The length of my_str is %li\n", strlen(my_str));
+    printf("My string my_str is %s\n\n", my_str);
 
     /* Second way */
     char my_str2[6];
     *my_str2 = 'H';
     my_str2[1] = 'E';
     my_str2[2] = 'L';
-    my_str2[3] = 'L';
-    *(my_str2 + 4) = 'O';
+    *(my_str2 + 3) = 'L';
+    my_str2[4] = 'O';
     my_str2[5] = '\0';
-    printf("My second string is %s\n", my_str2);
+    printf("My string my_str2 is %s\n\n", my_str2);
 
     /* Third way */
-    char my_str3[] = "Hello";
-    printf("My third string is %s\n", my_str3);
-
-    /* We can access and manipulate the string
-    characters, in the same way as arrays */
-    char c[6] = "Hello";
-    c[3] = 'K';
-    printf("Our string now is %s\n", c);
+    char my_str3[] = "HELLO";
     char *p;
-    p = c;
-    p[0] = 'Z';
-    *(p + 2) = 'W';
-    printf("Our string, modified from pointer is %s\n", c);
+    p = my_str3;
+    p[0] = 'W';
+    *(p + 2) = 'P';
+    printf("My string my_str3 is %s\n\n", my_str3);
+    modify_string(my_str3);
+    printf("My modified string my_str3 is %s\n\n", my_str3);
 
-    /* Like arrays, a string is passed as a reference in
-    the function, so we can modify it from the function */
-    modify_string(c);
-    printf("Our string modified from the function is %s\n", c);
+
+
 }
