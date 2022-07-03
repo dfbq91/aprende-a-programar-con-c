826090d182343fb89b64149a557961ca66bc968d
dfbq91 <dfbq91@gmail.com>
2022-07-02 22:32:43 -0500

    Memory array

---
+++ dynamic-memory/MemoryArray.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * Allocated 3 bytes in heap
+ * @return 0
+ * */
+int main() {
+    char *ptr = malloc(3 * sizeof *ptr);
+    *(ptr + 0) = 'H'; // ptr[0] = 'H'
+    ptr[1] = 'E';
+    ptr[2] = 'L';
+    printf("First letter: %c\nSecond Letter: %c\nThird letter: %c\n", ptr[0], ptr[1], ptr[2]);
+    return 0;
+}
